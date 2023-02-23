26d93f295706669b0e2a8b32c0c3d78f52e48c1a
sillyboylawliet <sillyboy_lch@foxmail.com>
2023-02-23 13:09:09 +0800

    update

--- build.bat
+++ build.bat
@@ -2,4 +2,12 @@ go env -w GOARCH=arm
 go env -w GOARM=7
 go env -w GOOS=linux
 go env -w CGO_ENABLED=0
-go build -ldflags "-s -w" -buildvcs=false  -o keepLogin
+go build -ldflags "-s -w" -buildvcs=false  -o linux-armv7-keepLogin
+
+go env -w GOARCH=amd64
+go env -w GOOS=linux
+go build -ldflags "-s -w" -buildvcs=false  -o linux-amd64-keepLogin
+
+go env -w GOARCH=amd64
+go env -w GOOS=windows
+go build -ldflags "-s -w" -buildvcs=false  -o windows-amd64-keepLogin.exe
